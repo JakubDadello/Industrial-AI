63425ae201ec6df644104115f470cf73a69a2ddf
Jakub Dadello <kejdi16try@gmail.com>
2026-02-18 14:04:48 +0100

    update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import numpy as np
 import cv2
 from PIL import Image as PILImage
 
-MODEL_PATH =  "../temp_production_model/"
-model = tf.keras.models.load_model(MODEL_PATH)
+MODEL_PATH =  "./temp_production_model/"
+CLASSES = ["crazing", "inclusion", "patches", "pitted_surface", "rolled-in_scale", "scratches"]
 IMG_SIZE = (200, 200)
 
+model = tf.keras.models.load_model(MODEL_PATH)
 
 def predictions (img: PILImage.Image):
 
@@ -17,21 +18,24 @@ def predictions (img: PILImage.Image):
      # 2. OpenCV Enhancement
     img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
     img_bgr = cv2.GaussianBlur(img_bgr, (3, 3), 0)
-    img_rgb= cv2.cvtColor(img_bgr, cv2.COLOR_BAYER_BG2RGB)
+    img_rgb= cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
     img_res = cv2.resize(img_rgb, IMG_SIZE )
 
     # 3. Input Preparation
-    img_array = img_res.astype(np.float32)
+    img_array = img_res.astype(np.float32)/255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     # 4. Model Inference
-    preds = model.predict(img_array)[0]
+    preds = model.predict(img_array)
 
-    demo = gr.Interface(
-        fn=preds, 
-        inputs=gr.Image(type="pil"), 
-        outputs=gr.Label(num_top_classes=1)
-        )
+    # 5. Output Post-processing
+    return {CLASSES[i]: float(preds[0][i]) for i in range(len(CLASSES))}
+
+demo = gr.Interface(
+    fn=predictions, 
+    inputs=gr.Image(type="pil"), 
+    outputs=gr.Label(num_top_classes=3)
+    )
     
-    if __name__ == "__main__":
-        demo.launch()
+if __name__ == "__main__":
+    demo.launch()
